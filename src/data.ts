8e0d78733b9a5d943121919f39ff0bf0a34fbafd
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-06-12 17:43:40 +0530

    Frontend
    Frontend part of AMZ

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+import { Product } from "./types/Product";
+
+export  const sampleProducts: Product[] = [
+    {
+    name: 'Nike Slim shirt',
+    slug: 'nike-slim-shirt',
+    image: '../images/p1.jpg',
+    category: 'shirt',
+    brand: 'Nike',
+    price: 120,
+    countInStock: 10, 
+    description: 'high quality shirt',
+    rating: 4.5,
+    numReviews: 10
+    },
+    {
+    name: 'Adidas Slim shirt',
+    slug: 'Adidas-slim-shirt',
+    image: '../images/p2.jpg',
+    category: 'shirt',
+    brand: 'Adidas',
+    price: 100,
+    countInStock: 20, 
+    description: 'high quality shirt',
+    rating: 4.0,
+    numReviews: 10
+    },
+    {
+    name: 'Lacoste Free Pants',
+    slug: 'lacoste-free-pants',
+    image: '../images/p3.jpg',
+    category: 'pants',
+    brand: 'Lacoste',
+    price: 220,
+    countInStock: 0, 
+    description: 'high quality shirt',
+    rating: 4.8,
+    numReviews: 17
+    },
+    {
+    name: 'Nike Slim Pants',
+    slug: 'Nike-slim-pants',
+    image: '../images/p4.jpg',
+    category: 'pants',
+    brand: 'Nike',
+    price: 64,
+    countInStock:8, 
+    description: 'high quality shirt',
+    rating: 4.2,
+    numReviews: 16
+    },
+
+]
